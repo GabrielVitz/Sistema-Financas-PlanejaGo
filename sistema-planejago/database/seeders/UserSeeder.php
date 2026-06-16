2c004d4318b0702454df591fa2593f9158f4b389
IsabellaT29 <115107520+IsabellaT29@users.noreply.github.com>
2026-06-16 19:03:52 -0400

    fix: arruma seeders para os testes

--- sistema-planejago/database/seeders/UserSeeder.php
+++ sistema-planejago/database/seeders/UserSeeder.php
@@ -1,14 +1,12 @@
 <?php
 
 namespace Database\Seeders;
+
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
-
 
 class UserSeeder extends Seeder
 {
@@ -17,17 +15,16 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $User = User::create([
-            'name'              => 'Admin',
-            'email'             => 'admin@mail.com',
-            'email_verified_at' => Carbon::now(),
-            'password'          => Hash::make('12345678'), 
-            'data_nascimento'   => '1990-05-15',
-            'log_data_inclusao' => Carbon::now()->toDateString(),
-            'remember_token'    => Str::random(10),
-            'created_at'        => Carbon::now(),
-            'updated_at'        => Carbon::now(),
-        ]);
-
+        User::updateOrCreate(
+            ['email' => 'user@mail.com'], 
+            [
+                'name'              => 'Usuário CT04',
+                'email_verified_at' => Carbon::now(),
+                'password'          => Hash::make('12345678'), 
+                'data_nascimento'   => '1990-05-15',
+                'log_data_inclusao' => Carbon::now()->toDateString(),
+                'remember_token'    => Str::random(10),
+            ]
+        );
     }
 }
